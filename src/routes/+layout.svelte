7f59c9beafa5e1d9b5fa93ac47d800a90cbe70ea
Wasyl Mowczan <wasyl.mowczan@tsh.io>
2024-06-28 12:42:07 +0200

    Final version of the project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import '../styles/app.scss'
+</script>
+
+<svelte:head>
+  <title>OpenTypeLab</title>
+</svelte:head>
+
+<div class="layout">
+  <nav>
+    <h1>🔥 OpenTypeLab</h1>
+  </nav>
+
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style lang="scss">
+  h1 {
+    font-family: 'Lexend Deca', sans-serif;
+    font-size: 1.8rem;
+    color: var(--fg-200);
+    letter-spacing: 2px;
+  }
+
+  .layout {
+    height: 100%;
+    display: grid;
+    grid-template-rows: auto 1fr;
+    align-items: center;
+    padding: 2rem;
+  }
+</style>
